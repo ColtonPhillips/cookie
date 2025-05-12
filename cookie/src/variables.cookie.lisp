c2115e8beb6c8ef3c3abf278e7c0ee4536a82977
Colton Phillips <coltonjphillips@gmail.com>
2025-05-11 19:26:24 -0700

    more deleterious paff and fluff

--- cookie/src/variables.cookie.lisp
+++ cookie/src/variables.cookie.lisp
@@ -5,7 +5,9 @@ master = [disaster]
 discord_url = [http://www.example.com]
 
 // images
-IBGURL=[https://i.imgur.com/d6uHzC8.png]
+IBGURL=[https://i.imgur.com/dddLouu.png]
+
+// items
 ICoin=[https://i.imgur.com/01F5NDL.png]
 IPlaceKitten=[https://placecats.com/300/200]
 IPlaceKitten2=[https://placecats.com/300/300]
@@ -14,3 +16,4 @@ ICigarettes=[https://i.imgur.com/V0czQ98.png]
 IBurger=[https://i.imgur.com/fBOcVQg.png]
 IToonies=[https://i.imgur.com/NdGwzWj.png]
 IHand=[https://i.imgur.com/zJpRTs8.png]
+ISilverDollars=[https://i.imgur.com/WdM1ry0.png]
