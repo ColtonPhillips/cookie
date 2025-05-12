10938f903843738a7f5b5804b21abddb0bcc48ec
Colton Phillips <coltonjphillips@gmail.com>
2025-05-11 18:25:47 -0700

    more tosh

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Credits
 Plague Clicker - shy moai
 art: https://nulltale.itch.io/white-scape
+https://vurmux.itch.io/urizen-onebit-tileset
+https://yugg0.itch.io/pack-of-cigarettes-psx-ps1
--- cookie/src/variables.cookie.lisp
+++ cookie/src/variables.cookie.lisp
@@ -5,7 +5,12 @@ master = [disaster]
 discord_url = [http://www.example.com]
 
 // images
+IBGURL=[https://i.imgur.com/d6uHzC8.png]
 ICoin=[https://i.imgur.com/01F5NDL.png]
 IPlaceKitten=[https://placecats.com/300/200]
 IPlaceKitten2=[https://placecats.com/300/300]
-IBGURL=[https://i.imgur.com/Y3IKNcv.png]
+IShovel=[https://i.imgur.com/FDEif4M.png]
+ICigarettes=[https://i.imgur.com/V0czQ98.png]
+IBurger=[https://i.imgur.com/fBOcVQg.png]
+IToonies=[https://i.imgur.com/NdGwzWj.png]
+IHand=[https://i.imgur.com/zJpRTs8.png]
